30b977eb3ada06863c8e6ebed4daa9a2772507c0
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-10-10 09:06:30 -0300

    método inserir

--- src/Usuario.php
+++ src/Usuario.php
@@ -13,6 +13,29 @@ class Usuario{
         $this->conexao = Banco::conecta();
     }
 
+    /* Métodos para rotina de CRUD mp Banco */
+
+    // INSERT de usuário
+
+    public function inserir(){
+        $sql = "INSERT INTO usuarios(nome, email, senha, tipo) VALUES(:nome, :email, :senha, :tipo)";
+        
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindValue(":email", $this->email, PDO::PARAM_STR);
+            $consulta->bindValue(":senha", $this->senha, PDO::PARAM_STR);
+            $consulta->bindValue(":tipo", $this->tipo, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao inserir usuário: ".$erro->getMessage());
+        }
+    }
+
+    // --------------------------------
+
+    
+
     // --------------------------------
     
     public function getId(): int
@@ -21,11 +44,9 @@ class Usuario{
     }
 
    
-    public function setId(int $id): self
+    public function setId(int $id): void
     {
         $this->id = $id;
-
-        return $this;
     }
 
     // --------------------------------
@@ -36,11 +57,9 @@ class Usuario{
     }
 
     
-    public function setNome(string $nome): self
+    public function setNome(string $nome): void
     {
         $this->nome = $nome;
-
-        return $this;
     }
 
     // --------------------------------
@@ -50,11 +69,9 @@ class Usuario{
     }
 
     
-    public function setEmail(string $email): self
+    public function setEmail(string $email): void
     {
         $this->email = $email;
-
-        return $this;
     }
 
    // ---------------------------------
@@ -63,11 +80,9 @@ class Usuario{
         return $this->senha;
     }
 
-    public function setSenha(string $senha): self
+    public function setSenha(string $senha): void
     {
         $this->senha = $senha;
-
-        return $this;
     }
 
     // ---------------------------------
@@ -77,24 +92,11 @@ class Usuario{
     }
 
    
-    public function setTipo(string $tipo): self
+    public function setTipo(string $tipo): void
     {
         $this->tipo = $tipo;
-
-        return $this;
     }
 
    // -----------------------------------
-    public function getConexao(): PDO
-    {
-        return $this->conexao;
-    }
-
     
-    public function setConexao(PDO $conexao): self
-    {
-        $this->conexao = $conexao;
-
-        return $this;
-    }
 }
